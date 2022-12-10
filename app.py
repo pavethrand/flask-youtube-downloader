6a456cf255f8ddec0b179d83dd985b79594fb083
pavethran <pavethranerd@gmail.com>
2022-12-10 08:51:53 +0530

    downloading video

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request, send_file
+from pytube import YouTube
+
+
+app = Flask(__name__)
+
+@app.route('/download', methods=["GET","POST"])
+def result():
+        try:
+            youtube_url = request.form["u-text"]
+            download_path = YouTube(youtube_url).streams[0].download()
+            fname = download_path.split("//")[-1]
+            return send_file(fname, as_attachment=True)
+        except:
+            return "Video download failed!"
+
+    
+
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+
+
+if __name__ == '__main__':
+    app.run(debug=True)
