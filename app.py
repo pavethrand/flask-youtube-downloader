75eb168f8fe52c3aa8df8fd0ba4693f84292dd09
Pavethran D <104271185+pavethrand@users.noreply.github.com>
2022-12-10 10:08:17 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,34 +1,21 @@
 from flask import Flask, render_template, request, send_file
 from pytube import YouTube
 
-
 app = Flask(__name__)
 
 @app.route('/download', methods=["GET","POST"])
 def result():
         try:
-#             youtube_url = request.form["u-text"]
-#             download_path = YouTube(youtube_url).streams[0].download()
-#             fname = download_path.split("//")[-1]
-#             return send_file(fname, as_attachment=True)
-              youtubeObject = YouTube(request.form["u-text"])
-              youtubeObject = youtubeObject.streams.get_highest_resolution()
-              youtubeObject.download()
-              return send_file(fname, as_attachment=True)
+            youtube_url = request.form["u-text"]
+            download_path = YouTube(youtube_url).streams[0].download()
+            fname = download_path.split("//")[-1]
+            return send_file(fname, as_attachment=True)
         except:
             return "Video download failed!"
-
-    
 
 @app.route('/')
 def index():
     return render_template('index.html')
 
-
-
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-
